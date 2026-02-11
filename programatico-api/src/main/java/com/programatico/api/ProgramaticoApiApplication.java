6120a2856de9121a9e8dfd3303ffd03aef56f078
caio de souza ramos <caio.sr06@gmail.com>
2026-02-11 11:25:04 -0300

    chore(project): initial monorepo structure with Spring Boot 3.5 and React

---
+++ programatico-api/src/main/java/com/programatico/api/ProgramaticoApiApplication.java
@@ -0,0 +1,13 @@
+package com.programatico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaticoApiApplication.class, args);
+	}
+
+}
